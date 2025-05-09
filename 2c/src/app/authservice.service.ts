e88bd84b2e02a24135c37e928db86ed773808429
Shravani Joshi <shravnijoshi2212@gmail.com>
2025-05-09 10:11:35 +0530

    new jquery

--- 2c/src/app/authservice.service.ts
+++ 2c/src/app/authservice.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+////Angular injects services into components as needed
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class AuthserviceService {
 
    users:any[]=[]
@@ -31,3 +35,12 @@ export class AuthserviceService {
     return this.loggedInUser;
   }
 }
+
+
+
+
+ //!!user is a double negation used to convert any value to a boolean (true or false).
+//!!user === Boolean(user)
+//It does not check if the value is strictly true or false, but whether it is "truthy" or "falsy"
+
+//This creates an Angular service class, which will be injected into components that need to use authentication features.
